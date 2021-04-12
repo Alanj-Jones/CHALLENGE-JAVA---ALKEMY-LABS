4c4d88c3bfce44c1b692305b02ae53b753e7bf92
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-12 19:56:29 -0300

    Project Initialization

---
+++ University_Simulator/src/main/java/com/challenge/alkemy/UniversitySimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.challenge.alkemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//Quitar los parentesis cuando configuremos la base de datos.
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class UniversitySimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitySimulatorApplication.class, args);
+	}
+
+}
