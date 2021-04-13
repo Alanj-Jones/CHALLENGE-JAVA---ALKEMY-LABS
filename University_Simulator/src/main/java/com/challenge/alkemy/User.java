c1f9f4fe7924deb34d58bf596c74bae536038b9c
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-13 19:17:32 -0300

    User class mapped

--- University_Simulator/src/main/java/com/challenge/alkemy/User.java
+++ University_Simulator/src/main/java/com/challenge/alkemy/User.java
@@ -2,14 +2,30 @@ package com.challenge.alkemy;
 
 public class User {
 
-    private int user_id;
-    private Role user_role;
+    private int userId;
+    private Role userRole;
     
     public User(){}
 
     public User(Role role, int id) {
-        this.user_id = id;
-        this.user_role = role;
-
+        this.userId = id;
+        this.userRole = role;
     }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Role getUserRole() {
+        return this.userRole;
+    }
+
+    public void setUserRole(Role userRole) {
+        this.userRole = userRole;
+    }
+    
 }
