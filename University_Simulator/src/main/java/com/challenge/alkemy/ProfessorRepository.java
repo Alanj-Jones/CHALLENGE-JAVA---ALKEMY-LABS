3716deee83b518884c5483bed2f687bd9ed99394
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-26 03:20:10 -0300

    Visuzalize professor name on subject table

--- University_Simulator/src/main/java/com/challenge/alkemy/ProfessorRepository.java
+++ University_Simulator/src/main/java/com/challenge/alkemy/ProfessorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProfessorRepository extends CrudRepository<Professor, Integer> {
     
-
+    public Professor findByProfessorId(Integer professor_id);
     
 }
