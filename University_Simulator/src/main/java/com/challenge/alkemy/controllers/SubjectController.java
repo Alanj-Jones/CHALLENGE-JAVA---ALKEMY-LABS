5438a09a4d095a60f4d79a1d51db6513fa9e60c9
Alan Jones <alanj.jones@comunidad.ub.edu.ar>
2021-04-27 05:22:53 -0300

    Habilidad de borrar materias

--- University_Simulator/src/main/java/com/challenge/alkemy/controllers/SubjectController.java
+++ University_Simulator/src/main/java/com/challenge/alkemy/controllers/SubjectController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -40,11 +41,18 @@ public class SubjectController {
     }
 
     @PostMapping("/Admin/subjectCreated")
-    private String subjectCreation(@Valid @ModelAttribute(value = "subj") Subject subject, BindingResult result) {
+    private String subjectCreation(@Valid @ModelAttribute(value = "subj") Subject subject, BindingResult result, Model model) {
+        model.addAttribute("professors", professorRepo.findAll());
         if (result.hasErrors()) {
             return "addSubject";
         }
         subjectRepo.save(subject);
+        return "redirect:/subjects";
+    }
+
+    @GetMapping("/Admin/subject/delete/{id}")
+    public String deleteSubject(@PathVariable(name = "id") Integer id) {
+        subjectRepo.deleteById(id);
         return "redirect:/subjects";
     }
 }
